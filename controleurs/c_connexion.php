274104f709f4a0fd5b9f99cc23cdf6efb2ba10e2
aMajnoun <amajnoun@gmail.com>
2016-12-03 16:58:59 +0100

    Merge origin/master
    Conflicts:
    	client/index.php
    	nbproject/project.xml
    	serveur/index.php

---
+++ controleurs/c_connexion.php
@@ -0,0 +1,57 @@
+<?php
+if(!isset($_REQUEST['action'])){
+	$_REQUEST['action'] = 'demandeConnexion';
+}
+$action = $_REQUEST['action'];
+switch($action){
+	case 'demandeConnexion':{
+		include("vues/v_connexion.php");  
+		break;
+	}
+	case 'valideConnexion':{
+		$login = $_REQUEST['login'];
+		$mdp = $_REQUEST['mdp'];
+                
+                               
+            $info =$pdo->getInfosVisiteur($login,$mdp);
+           
+             if($info['type']== 'V')
+             {
+                 $visiteur = $pdo->getConnexionVisiteur($login,$mdp);
+                        $id = $visiteur['idVisit'];
+			$nom =  $visiteur['nom'];
+			$prenom = $visiteur['prenom'];
+                        $derniereCo = $visiteur['derniereConnexion'];
+			 $type = $visiteur['type'];
+			connecter($id,$nom,$prenom,$type,$derniereCo);
+			
+                        $pdo->UpdateDate($id);
+                        include("vues/v_sommaire.php");
+		
+             }    
+             else if ($info['type']== 'C'){
+                 $comptable = $pdo->getConnexionComptable($login,$mdp);
+			$id = $comptable['id'];
+			$nom =  $comptable['nom'];
+			$prenom = $comptable['prenom'];
+                        $type = $comptable['type'];
+                        $derniereCo = $comptable['derniereConnexion'];
+                        
+			connecter($id,$nom,$prenom,$type,$derniereCo);
+			
+                        $pdo->UpdateDate($id);
+                        include("vues/v_sommaire.php");
+		
+                }
+            else{               
+			ajouterErreur("Login ou mot de passe incorrect","connexion");
+            }
+            
+            break;
+	}
+	default :{
+		include("vues/v_connexion.php");
+		break;
+	}
+}
+?>
---
+++ vues/v_sommaire.php
@@ -0,0 +1,63 @@
+    <!-- Division pour le sommaire -->
+<div class="row">
+      
+    <nav class='col-md-2'>
+        
+        <h4>
+            <?php echo $_SESSION['prenom']."  ".$_SESSION['nom']?>
+            
+        </h4>
+           
+        <ul class="list-unstyled">
+			
+           <li>
+              <a href="index.php?uc=gererFrais&action=saisirFrais" title="Saisie fiche de frais ">Saisie fiche de frais</a>
+           </li>
+           <li>
+              <a href="index.php?uc=etatFrais&action=selectionnerMois" title="Consultation de mes fiches de frais">Mes fiches de frais</a>
+           </li>
+           
+           <!-- Si l'utilisateur est un comptable alors afficher cette fonctionnalité -->
+           <?php 
+            if($_SESSION['type'] == 'C')
+            {
+           ?>
+           
+           <li>
+              <a href="index.php?uc=validerFrais&action=afficherFormulaireValidationFicheFrais" title="Validation de fiche de frais">Valider fiche de frais</a>
+           </li>
+           
+           <?php        
+          }
+           ?>          
+           <?php
+          // $info =$pdo->getInfosVisiteur($login,$mdp);
+           if($_SESSION['type'] == 'C')
+           {
+           ?>
+           <li>
+              <a href="index.php?uc=inscriptionNouveauVisiteur&action=demandeInscription" title="inscription">Inscription nouveau visiteur</a>
+           </li>
+           <?php
+           }
+           ?>
+            <?php 
+           if($_SESSION['type'] == "C")
+          {
+               ?>
+           <li>
+              <a href="index.php?uc=suivrePaiement&action=suivrePaiement" title="Se déconnecter">Suivie paiement fiche frais</a>
+           </li>
+        <?php }?>
+ 	   <li>
+              <a href="index.php?uc=connexion&action=deconnexion" title="Se déconnecter">Déconnexion</a>
+           </li>
+          
+         </ul>
+        
+    </nav>
+    <div id="contenu" class="col-md-10">
+   
+        
+    
+    
