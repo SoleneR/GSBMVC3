93faf2a3bc6aff6285a29a14f7001f4fcd35581c
SoleneR <solene.renoud@orange.fr>
2016-12-05 09:35:37 +0100

    Merge origin/master
    Conflicts:
    	vues/entetePage.html
    	vues/pageAgendaJour.php
    	vues/pageAjoutReserv.php
    	vues/pageConnexion.php

--- index.php
+++ index.php
@@ -19,6 +19,9 @@ switch($uc){
 	case 'etatFrais' :{
 		include("controleurs/c_etatFrais.php");break; 
 	}
+        case 'inscriptionNouveauVisiteur' :{
+                include("controleurs/c_inscriptionVisiteur.php");break;         
+        }
 }
 include("vues/v_pied.php") ;
 ?>
